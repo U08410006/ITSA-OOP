d1f3bc6c96641cd0b28acfe20b41ba5612b33302
U08410006 <55177753+U08410006@users.noreply.github.com>
2021-06-27 22:03:08 +0800

    test

---
+++ 新增資料夾 (2)/1.c
@@ -0,0 +1,11 @@
+int main(){  
+    int a[10] , i ;  
+    for(i = 0 ; i < 6 ; i ++){  
+        scanf("%d",&a[i]);  
+    }  
+    for(i = 5 ; i > 0 ; i --){  
+        printf("%d ",a[i]);  
+    }printf("%d\n",a[i]);  
+      
+return 0;  
+}  
---
+++ 新增資料夾 (2)/1.cpp
@@ -0,0 +1,13 @@
+#include <iostream> 
+using namespace std;
+int main(){  
+    int a[10] , i ;  
+    for(i = 0 ; i < 6 ; i ++){  
+        cin >> a[i];  
+    }  
+    for(i = 5 ; i > 0 ; i --){  
+        cout << a[i] << " ";  
+    }cout << a[i] << "\n";  
+      
+return 0;  
+}  
